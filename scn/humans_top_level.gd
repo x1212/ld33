b8a837c9a1b1abd7efdd719fcc71d4e2021a2e90
x1212 <x1212-x@web.de>
2015-08-22 22:01:39 +0200

    preparations for adding humans

---
+++ scn/humans_top_level.gd
@@ -0,0 +1,20 @@
+
+extends Spatial
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+var resources
+
+func add_resource( type ):
+	pass
+
+func get_food():
+	return false
+
+func _ready():
+	# Initialization here
+	pass
+
+
