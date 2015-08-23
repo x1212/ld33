e997244dfa076659538491e2bcbf95a9d5135fa1
x1212 <x1212-x@web.de>
2015-08-23 19:21:04 +0200

    scaling buttons

--- scn/gui_buttons.gd
+++ scn/gui_buttons.gd
@@ -5,10 +5,19 @@ extends HBoxContainer
 # var a=2
 # var b="textvar"
 
+var scale = 1.0
+
 func _ready():
 	# Initialization here
 	set_process(true)
 	pass
 
 func _process(delta):
-	set_pos( Vector2(0,OS.get_video_mode_size().y-get_size().y) )
+	if (OS.get_video_mode_size().y <= 600 ):
+		scale=600.0/OS.get_video_mode_size().y
+	else:
+		scale=1.0
+	set_pos( Vector2(0,OS.get_video_mode_size().y-get_size().y/scale) )
+	for i in range( get_child_count()):
+		if ( get_child(i) extends TextureButton ):
+			get_child(i).set_scale(Vector2(1.0/scale,1.0/scale))
